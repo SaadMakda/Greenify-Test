d2a5648e13f82f992305fb7617f4d0493188f0d0
Saad Makda <saadiosop@gmail.com>
2024-11-17 01:19:12 -0600

    Input fields

--- src/components/Dashboard/Grid.tsx
+++ src/components/Dashboard/Grid.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { StatCards } from "./StatCards";
-import { ActivityGraph } from "./ActivityGraph";
+import { ActivityInputs } from "./ActivityInputs";
 import { UsageRadar } from "./UsageRadar";
 import { RecentTransactions } from "./RecentTransactions";
 
 export const Grid = () => {
   return (
-    <div className="px-4 grid gap-3 grid-cols-12">
-      <StatCards />
-      <ActivityGraph />
-      <UsageRadar />
-      <RecentTransactions />
+    <div className="px-4 grid gap-6 grid-cols-12">
+      {/* Adjust column span here */}
+      <div className="col-span-12 lg:col-span-6">
+        <ActivityInputs />
+      </div>
+      <div className="col-span-12 lg:col-span-6">
+        <UsageRadar />
+      </div>
+      <div className="col-span-12">
+        <RecentTransactions />
+      </div>
     </div>
   );
 };
